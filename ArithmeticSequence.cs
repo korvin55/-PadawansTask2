e26708f0000f8f0cb572b9abcc5cf39cc999a493
korvin <47597209+korvin55@users.noreply.github.com>
2019-02-15 22:02:56 +0300

    /

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,9 +6,13 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            if ( count < 0)
+            if ( count < 0 || number == int.MaxValue)
             {
                 throw new ArgumentException();
+            }
+            if (number == int.MinValue && add < 0)
+            {
+                throw new ArgumentOutOfRangeException();
             }
             int sum = 0;
             for (int step = 0; step < count; step++)
